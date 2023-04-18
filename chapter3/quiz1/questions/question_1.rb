76a8e28e4dcdf95e1cf18dcc05121c32b3dd6f65
makers-dev <dev@makersacademy.com>
2023-04-18 12:07:32 +0100

    Next quiz from Hub.

---
+++ chapter3/quiz1/questions/question_1.rb
@@ -0,0 +1,18 @@
+# Uncomment each line and fill in the blanks so that the arithmetic
+# operations produce the correct value for each variable.  e.g. in
+# `four = 2.+()`, the calculation to the right of the `=` should
+# evaluate to 4. So it would become `four = 2.+(2)`.
+
+# five = 2.+()
+
+# seven = 14./()
+
+# two = 2.*()./()
+
+# three_point_zero = 6.to_f./()
+
+# two_is_integer = 4.to_f./().to_i.integer?
+
+# four = 12.%()
+
+# zero = 12.%()
