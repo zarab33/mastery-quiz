1405b847770c86fce5c94d171075ccb178c0af64
Zara <zarabucknor@yahoo.co.uk>
2023-04-18 13:29:05 +0100

    chapter3

--- chapter3/quiz1/questions/question_1.rb
+++ chapter3/quiz1/questions/question_1.rb
@@ -3,16 +3,16 @@
 # `four = 2.+()`, the calculation to the right of the `=` should
 # evaluate to 4. So it would become `four = 2.+(2)`.
 
-# five = 2.+()
+five = 2.+(3)
 
-# seven = 14./()
+seven = 14./(2)
 
-# two = 2.*()./()
+two = 2.*(2)./(2)
 
-# three_point_zero = 6.to_f./()
+three_point_zero = 6.to_f./(2)
 
-# two_is_integer = 4.to_f./().to_i.integer?
+two_is_integer = 4.to_f./(2).to_i.integer?
 
-# four = 12.%()
+four = 12.%(8)
 
-# zero = 12.%()
+zero = 12.%(4)
